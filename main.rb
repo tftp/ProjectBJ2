aa4b99cf2b9742fc2e75250959315f1d27da832e
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-18 20:48:48 +0300

    corrected main

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
+require_relative 'game'
+require_relative 'terminal_interface'
+require_relative 'player'
+require_relative 'dealer'
+
 class BlackJack
 
   def initialize
     player = Player.new('OS')
     dealer = Dealer.new
 
+    game = Game.new(player, dealer)
+
+    TerminalInterface.new(game)
   end
 
 end
