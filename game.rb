f0cd23f380fecc87b8e129174e4a296e2244dc61
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-18 19:38:14 +0300

    add game logica

---
+++ game.rb
@@ -0,0 +1,21 @@
+class Game
+
+  attr_reader :status, :actions
+
+  def initialize(player,dealer)
+
+  end
+
+  def actions
+
+  end
+
+  def player_cards
+
+  end
+
+  def dealer_cards
+
+  end
+
+end
