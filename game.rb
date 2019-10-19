ec5487d7c34be70b3641c162dac2ba9668be329f
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-19 13:28:37 +0300

    initialize deck

--- game.rb
+++ game.rb
@@ -1,9 +1,12 @@
 class Game
-
+  include CardOptions
   attr_reader :status, :actions
 
   def initialize(player,dealer)
-
+    @player = player
+    @dealer = dealer
+    @cards = []
+    @deck = Deck.new(create_deck_of_cards)
   end
 
   def actions
