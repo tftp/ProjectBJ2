02e20c9bc8e7fc502ef9e1ad4632dedbf7eaa997
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-20 15:35:35 +0300

    any corrections

--- game.rb
+++ game.rb
@@ -10,21 +10,36 @@ class Game
     @player = player
     @dealer = dealer
     @cards = []
-    @deck = Deck.new(create_deck_of_cards)
+    @deck
     @bank = 0
   end
 
-  def actions
+  def actions(value)
+    case value
+    when '1'
+      choise_of_dealer
+      false
+    when '2'
+      add_cards_player
+      choise_of_dealer
+      true
+    when '3'
+      choise_of_dealer
+      true
+    end
+  end
 
+  def new_deck
+    @deck = Deck.new(create_deck_of_cards)
   end
 
   def add_cards_player
     if @player.cards.empty?
       @bank += 10
       @player.change_bank(RATE)
-      2.times {@player.add_card(@deck.cards.delete_at(rand(0..51)))}
+      2.times {@player.add_card(@deck.cards.delete_at(rand(0..@deck.cards_number)))}
     elsif @player.cards.count < 3
-      @player.add_card(@deck.cards.delete_at(rand(0..51)))
+      @player.add_card(@deck.cards.delete_at(rand(0..@deck.cards_number)))
     end
   end
 
@@ -32,9 +47,9 @@ class Game
     if @dealer.cards.empty?
       @bank += 10
       @dealer.change_bank(RATE)
-      2.times {@dealer.add_card(@deck.cards.delete_at(rand(0..51)))}
+      2.times {@dealer.add_card(@deck.cards.delete_at(rand(0..@deck.cards_number)))}
     elsif @dealer.cards.count < 3
-      @dealer.add_card(@deck.cards.delete_at(rand(0..51)))
+      @dealer.add_card(@deck.cards.delete_at(rand(0..@deck.cards_number)))
     end
   end
 
@@ -44,6 +59,16 @@ class Game
 
   def dealer_cards
     @dealer.points_of_cards(HIDE)
+  end
+
+  def choise_of_dealer
+    count_of_cards = @dealer.cards.count
+    if @dealer.points < 17 && count_of_cards < 3
+      add_cards_dealer
+      puts "\nИгрок #{@dealer.name} взял карту."
+    else
+      puts "\nИгрок #{@dealer.name} не взял карту."
+    end
   end
 
 end
